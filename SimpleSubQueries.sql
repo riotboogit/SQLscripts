6b5d791301c741b0973d0cf6fd3c973fc739d5dc
Suzanne <riotboo@gmail.com>
2025-03-19 11:03:54 -0400

    Update SimpleSubQueries.sql

--- SimpleSubQueries.sql
+++ SimpleSubQueries.sql
@@ -6,3 +6,13 @@ From [dbo].[LPM] where try_cast(NewYear1BR as decimal(10,2))>=
 	SELECT Avg(try_cast(NewYear1_baseRent as decimal(10,2)))
   FROM [dbo].[LPM]
    )
+
+-- adpply a filter for the avg based on execution date
+
+Select [Tenant],[Space], try_cast( NewYear1BR as decimal(10,2)) as NewYear1BR_Num [Execution_Date]
+From [dbo].LPM] where try_cast(NewYear1BR as decimal(10,2))>= 
+(
+  SELECT Avg(try_cast(NewYear1_baseRent as decimal(10,2)))
+  FROM [dbo].[LPM] where Try_cast([Execution_Date] as date) > '01/01/2025'
+   )
+
