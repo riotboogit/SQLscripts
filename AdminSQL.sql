a6bd89a41d95a15cf16bd21776d570823c0d9464
Suzanne <riotboo@gmail.com>
2026-09-08 13:30:22 -0400

    Add readonly server role and user assignment
    Created a readonly server role and assigned an AD user with necessary permissions.

--- AdminSQL.sql
+++ AdminSQL.sql
@@ -6,3 +6,26 @@ INNER JOIN
 sys.server_principals P
 ON J.owner_sid = P.sid
 GO
+
+
+--create readonly server role and assign ad user
+USE [master]
+GO
+
+CREATE LOGIN [CORPORATE\ADUserName] FROM WINDOWS WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english]
+GO
+
+CREATE SERVER ROLE [ReadOnlyAccess] AUTHORIZATION [securityadmin];
+
+
+-- Allow the role to connect to any database
+GRANT CONNECT ANY DATABASE TO [ReadOnlyAccess];
+
+-- Grant the ability to view any database definition
+GRANT VIEW ANY DEFINITION TO [ReadOnlyAccess];
+
+-- Optionally, grant VIEW ANY DATABASE to see all databases
+GRANT VIEW ANY DATABASE TO [ReadOnlyAccess];
+
+-- Replace 'YourUser' with the actual login name
+ALTER SERVER ROLE [ReadOnlyAccess] ADD MEMBER [CORPORATE\ADUserName];
